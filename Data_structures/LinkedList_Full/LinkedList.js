738199cecda1c5e3088d279b622b7185b09eb93a
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-04-10 17:48:22 +0200

    final

--- Data_structures/LinkedList_Full/LinkedList.js
+++ Data_structures/LinkedList_Full/LinkedList.js
@@ -42,6 +42,73 @@ class LinkedList {
       
       return str;
     }
+
+    print(node){
+      console.log(node.element);
+      if(node.next == null){
+        return;
+      }
+      return this.print(node.next);  
+    }
+
+    /**
+     * 
+     * @param {Node} element 
+     * @param {Int} index (default is 0)
+     * la funzione inserisce un elemento ad un indice preciso, se l'indice non viene specificato
+     * l'elemento viene inserito di default alla posizione head
+     */
+    insert(element, index = 0){
+      if(index < 0 || index > this.size) return false;
+
+      const node = this.createNode(element);  // creo un nuovo oggetto nodo
+      
+      //caso in cui l'inserzione è in testa
+      if(index === 0){
+        //this.push(element); inserisce l'elemento nell'ultima posizione
+        node.next = this.head;  //faccio puntare il prossimo elemento del nuovo elemento alla 'vecchia' testa della lista
+        this.head = node;       //aggiorno la lista 
+      }else{
+        let previous = this.head; //se non entro nel loop
+
+        for(let i=0; i<index -1; i++){
+          previous = previous.next;
+        }
+
+        node.next = previous.next;
+        previous.next = node;
+      }
+
+      this.size +=1; //inserisco quindi aumento il size
+      return true;  //ritorno true quando l'operazione va a buon fine.
+    }
+    
+
+    get(index){
+      if(index === undefined || index <0 || index > this.size) return  null;
+      if(index == 0) return this.head.element;  //ritorno l'elemento e non l'oggetto
+
+      let current = this.head;
+
+      for(let i=0; i<index; i++){
+        current = current.next;
+      }
+
+      return current.element;
+    }
+
+    indexOf(element){
+      let current = this.head;
+      if(this.head.element === element) return 0;
+
+      for(let i=0; i<this.size; i++){
+        if(current.element === element){
+          return i;
+        }
+        current = current.next;
+      }
+      return null;
+    }
   }
 
   const list = new LinkedList();
@@ -52,4 +119,14 @@ class LinkedList {
   list.push(1);
   list.push(10);
 
-  console.log(list.toString());
+  //list.insert(50);
+
+  //console.log(list.toString());
+  //list.print(list.head);
+  
+  
+  /*console.log(list.get(0));
+  console.log(list.get(1));
+  console.log(list.get(2));*/
+
+  console.log(list.indexOf(20));
