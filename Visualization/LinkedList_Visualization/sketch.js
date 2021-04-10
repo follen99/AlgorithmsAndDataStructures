74f4e4a720d75c71efb1659d8ab38f87589b8ad6
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-04-10 19:57:56 +0200

    completo

--- Visualization/LinkedList_Visualization/sketch.js
+++ Visualization/LinkedList_Visualization/sketch.js
@@ -7,25 +7,25 @@ let rectSize = 30;
 let flag = true;
 
 
-let removeHeadButton;
-let removeEndButton;
-let addHeadButton;
-let removeHeadButton;
-let removeAtRandomButton;
-let addAtRandomButton;
+
+var removeHeadButton;
+var removeEndButton;
+var addHeadButton;
+var removeAtRandomButton;
+var pushButton;
 
 function setup() {
   createCanvas(420, 400);
 
   list = new LinkedList();  //creo la lista
 
-  for(let i=0; i<20; i++){
-    list.push(floor(random(0,100)));
+  for (let i = 0; i < 24; i++) {
+    list.push(floor(random(0, 100)));
     list.get(i).x = defX;
     list.get(i).y = defY;
 
-    defX+=50;
-    if(defX >= width){
+    defX += 50;
+    if (defX >= width) {
       defX = 20;
       defY += 75;
     }
@@ -47,44 +47,87 @@ function setup() {
   }
 
 
-  /*list.push(20);
-  list.get(0).x = 20;
-  list.get(0).y = 20;*/
+
+  removeHeadButton = createButton("Remove head");
+  removeEndButton = createButton("Remove end");
+  //addHeadButton = createButton("Push at head");
+  removeAtRandomButton = createButton("Remove random idex");
+  pushButton = createButton("Push");
+
+  removeHeadButton.mousePressed(removeHead);
+  removeEndButton.mousePressed(removeEnd);
+  //addHeadButton.mousePressed(addHead);
+  removeAtRandomButton.mousePressed(removeRandom);
+  pushButton.mousePressed(pushFunction);
+
+
+}
+
+function removeHead(){
+  list.removeAt(0);
+}
+
+function removeEnd(){
+  list.removeAt(list.size-1);
+}
+
+function addHead(){
   
-  //console.log();
-  //list.print(list.head);
 
-  //console.log(list.get(0).next.x);
+  var index = list.size;
 
-  //list.removeAt(list.size-1);
+  list.insert("new",0);
+  list.get(index).x = 0;
+  list.get(index).y = 0;
+  //aggiungi coordinate
+}
+
+function removeRandom(){
+  var randomIndex = floor(random(0, list.size-1));
+  list.removeAt(randomIndex);
+}
+
+function pushFunction(){
+  //list.push("new");
+  var index = list.size;
+
+  list.insert("new", index);
+  list.get(index).x = defX;
+    list.get(index).y = defY;
+
+    defX += 50;
+    if (defX >= width) {
+      defX = 20;
+      defY += 75;
+    }
 }
 
 function draw() {
   background(220);
-  for(let i=0; i<list.size; i++){
+  for (let i = 0; i < list.size; i++) {
 
     let currentObject = list.get(i);
 
-    rect(list.get(i).x,list.get(i).y,rectSize);
+    rect(list.get(i).x, list.get(i).y, rectSize);
 
     textAlign(CENTER);
-    text(currentObject.element,(currentObject.x)+(rectSize/2),(currentObject.y)+(rectSize/2));
+    text(currentObject.element, (currentObject.x) + (rectSize / 2), (currentObject.y) + (rectSize / 2));
 
     //line(list.get(i).x, list.get(i).y, 40,40 /*list.get(i).next.x, list.get(i).next.x*/);
     strokeWeight(2);
-    if(list.get(i).next != null){
-      line(currentObject.x+rectSize, currentObject.y+rectSize/2, currentObject.next.x,currentObject.next.y+rectSize/2);
-      ellipse(currentObject.next.x,currentObject.next.y+rectSize/2,10);
+    if (list.get(i).next != null) {
+      line(currentObject.x + rectSize, currentObject.y + rectSize / 2, currentObject.next.x, currentObject.next.y + rectSize / 2);
+      ellipse(currentObject.next.x, currentObject.next.y + rectSize / 2, 10);
     }
-    
-    
-  
+
+
+
   }
   /*
   let v0 = createVector(list.get(0).x+rectSize, list.get(0).y+rectSize/2);
   let v1 = createVector(list.get(0).next.x,0);
   drawArrow(v0, v1, 'black');*/
-  
+
 }
 
 function drawArrow(base, vec, myColor) {
