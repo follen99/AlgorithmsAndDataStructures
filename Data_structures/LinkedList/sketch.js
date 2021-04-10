9418723c37a77b1d43334602e41b60d36d562766
Giuliano Ranauro <giulianoranauroiphone1@gmail.com>
2021-04-10 13:32:41 +0200

    basic structure

--- Data_structures/LinkedList/sketch.js
+++ Data_structures/LinkedList/sketch.js
@@ -1,7 +1,22 @@
 function setup() {
-  createCanvas(400, 400);
+  //createCanvas(400, 400);
+  noCanvas();
+  var node1= new Node();
+  var node2= new Node();
+
+  node1.item = "1";
+  node2.item = "2";
+
+  node1.next=node2;
+
+  console.log(node1);
 }
 
-function draw() {
+function Node(){
+  this.item = null;
+  this.next = null;
+};
+
+/*function draw() {
   background(220);
-}
+}*/
